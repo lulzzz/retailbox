f781758fe8436be21f322929ae9d748bb00d6e04
Mohsin Baig <moebagels@gmail.com>
2018-06-29 08:26:26 -0500

    updated output

--- src/output.py
+++ src/output.py
@@ -4,3 +4,41 @@ import re
 from colorama import init
 from termcolor import color
 
+init() # Termcolor support for win32
+
+#
+# Color Message Printing for Terminal
+#
+
+def printGreen(msg):
+    print(color(msg, 'green'))
+
+def printYellow(msg):
+    print(color(msg, 'yellow'))
+
+def printMagenta(msg):
+    print(color(msg, 'magenta'))
+
+def printRed(msg):
+    print(color(msg, 'red'))
+
+
+#
+# Display Terminal Messages / Prompts
+#
+
+help_message = '''
+  🛍️ Machine Learning eCommerce Recommender System
+
+  Usage
+    $ retailbox [<options> ...]
+    
+'''
+
+def displayHelpMessage():
+    print(help_message)
+
+def displayVersion(version):
+    print('RetailBox' + version)
+
+
